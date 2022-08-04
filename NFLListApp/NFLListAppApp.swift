72496cdc7264a451ca2fe7da2c87d79169c8f75e
Khoi7 <s3694615@rmit.edu.vn>
2022-08-04 16:36:55 +0700

    Initial Commit

---
+++ NFLListApp/NFLListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFLListAppApp.swift
+//  NFLListApp
+//
+//  Created by DuyPhung on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NFLListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
